157f23c86d77de2d2e1390cdea259437eeb0316a
dheerendra2007 <dheerendra2007@gmail.com>
2022-05-27 10:43:05 +0530

    basic website structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule} from "@angular/router";
+import { AboutComponent } from "./pages/about/about.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { PortfolioComponent } from "./pages/portfolio/portfolio.component";
+import { ServicesComponent } from "./pages/services/services.component";
+
+const routes: Routes = [
+    {
+        path:"", component:HomeComponent
+    },
+    {
+        path:'about', component:AboutComponent
+    },
+    {
+        path:'contact', component:ContactComponent
+    },
+    {
+        path: 'services', component:ServicesComponent
+    },
+    {
+        path:'portfolio', component:PortfolioComponent
+    }
+];
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule{
+
+}
